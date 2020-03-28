0ec05497257bef6de1351f9e9cd112a8ca49f2d5
Paco <pacoswilliams@gmail.com>
2020-03-28 00:09:30 -0600

    client optimizations

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var http = require("http");
 var path = require("path");
+var WebSocket = require("ws");
 var socketIO = require("socket.io");
 var app = express();
 var server = http.Server(app);
-var io = socketIO(server);
+var io = socketIO(server, { transports: ["websocket"] });
 app.set("port", 5000);
 app.use(express.static(path.join(__dirname, "dist")));
 app.use("/static", express.static(__dirname + "/static")); // Routing
@@ -25,6 +26,40 @@ var exists = id => {
   else return false;
 };
 
+// const web = new WebSocket.Server({ server });
+
+// var players = {};
+
+// var generateID = storage => {
+//   //create 8 digit random id
+//   let str = "";
+//   for (let i = 0; i < 5; i++) {
+//     if (i % 2 == 0) str += Math.floor(Math.random() * 9);
+//     else str += String.fromCharCode(Math.floor(Math.random() * (89 - 65) + 65));
+//   }
+//   if (storage[str]) generateID();
+//   else return str;
+// };
+
+// web.on("connection", socket => {
+//   if (!socket.id) socket.id = generateID(players);
+//   players[socket.id] = {
+//     x: 0,
+//     y: 0
+//   };
+// });
+
+// const update = () => {
+//   web.clients.forEach(client => {
+//     client.send("ping");
+//   });
+// };
+
+// setInterval(() => {
+//   console.log(players);
+//   update();
+// }, 1000);
+
 var addPlayer = id => {
   if (!exists(id)) {
     var newPlayer = {
@@ -41,8 +76,10 @@ io.on("connection", socket => {
 
   addPlayer(id);
 
-  socket.on("fire", ({ position, target, userId, id }) => {
-    createBullet(position, target, userId, id);
+  socket.on("fire", array => {
+    let { position, target } = decode(array, "bullet");
+
+    createBullet(position, target, socket.id);
   });
 
   socket.on("disconnect", () => {
@@ -50,22 +87,26 @@ io.on("connection", socket => {
   });
 
   socket.on("moveRequest", input => {
-    moveLogic(input);
+    moveLogic(input, socket.id);
   });
 });
 
-const createBullet = (position, target, userId, id) => {
-  let { angle, position: pos } = setAngle(position, target);
-  projectiles[id] = { angle, position: pos, id, damage: 10, userId };
+const createBullet = (position, target, userID) => {
+  let angle = setAngle(position, target);
+  var ID = generateID();
+  position.radius = 2;
+  position.x += Math.floor(angle.x * 20);
+  position.y += Math.floor(angle.y * 20);
+  projectiles[ID] = { position, angle, userID };
 };
 
-const moveLogic = input => {
+const moveLogic = (input, id) => {
   let speed = 3;
-  let player = players[input.playerID];
+  let player = players[id];
   if (!player) return;
 
-  player.position.x += input.pressTimeX * speed * 100;
-  player.position.y += input.pressTimeY * speed * 100;
+  player.position.x += Math.floor(input.pressTimeX * speed * 100);
+  player.position.y += Math.floor(input.pressTimeY * speed * 100);
   player.lastCompletedSequence = input.sequenceID;
 };
 
@@ -73,9 +114,14 @@ const moveLogic = input => {
 
 const updateProjectiles = () => {
   var speed = 70;
+
   for (let id in projectiles) {
-    // delete projectiles[id];
-    let { x, y } = projectiles[id].position;
+    let curr = projectiles[id];
+    let { position, angle } = curr;
+
+    var { x: aX, y: aY } = angle;
+    let { x, y } = position;
+
     if (
       x < 0 ||
       x > gameDimensions.width ||
@@ -84,12 +130,8 @@ const updateProjectiles = () => {
     ) {
       delete projectiles[id];
     } else {
-      projectiles[id].position.x += Math.floor(
-        projectiles[id].angle.angleX * speed
-      );
-      projectiles[id].position.y += Math.floor(
-        projectiles[id].angle.angleY * speed
-      );
+      curr.position.x += Math.floor(aX * speed);
+      curr.position.y += Math.floor(aY * speed);
     }
   }
 };
@@ -97,7 +139,7 @@ const updateProjectiles = () => {
 const checkHits = () => {
   for (let userID in players) {
     for (let id in projectiles) {
-      if (projectiles[id].userId != userID) checkBulletHit(userID, id);
+      if (projectiles[id].userID != userID) checkBulletHit(userID, id);
     }
   }
 };
@@ -117,13 +159,10 @@ const checkBulletHit = (playerId, bulletId) => {
 
   let distance = Math.sqrt(dx * dx + dy * dy);
 
-  // console.log(
-  //   `distance: ${distance}  radiusSum: ${bulletPos.radius + playerPos.radius}`
-  // );
-
-  if (distance <= bulletPos.radius + playerPos.radius) {
-    player.health -= bullet.damage;
+  if (distance <= bulletPos.radius + playerPos.radius + 10) {
+    player.health -= 10;
     if (player.health <= 0) delete players[playerId];
+
     delete projectiles[bulletId];
   }
 };
@@ -134,20 +173,60 @@ var setAngle = (position, target) => {
 
   let angle = Math.atan2(deltaY, deltaX);
 
-  let angleX = Math.cos(angle);
-  let angleY = Math.sin(angle);
+  let x = Math.cos(angle);
+  let y = Math.sin(angle);
 
-  let x = position.x + 1 * Math.cos(angle);
-  let y = position.y + 1 * Math.sin(angle);
-
-  return { angle: { angleX, angleY }, position: { x, y, radius: 2 } };
+  return { x, y };
 };
 
 //send updates
 
 setInterval(() => {
+  let timeStamp = +new Date();
   updateProjectiles();
   checkHits();
-  let timeStamp = +new Date();
-  io.sockets.emit("state", { players, projectiles, timeStamp });
-}, 1000 / 30);
+
+  var encoded = encodeProjectiles();
+  io.sockets.emit("state", { players, projectiles: encoded, timeStamp });
+}, 1000 / 10);
+
+var decode = (array, type) => {
+  if (type == "bullet") {
+    let [xI, yI, xF, yF] = array;
+    return { position: { x: xI, y: yI }, target: { x: xF, y: yF } };
+  } else if (type == "player") {
+    let [health, x, y] = array;
+    return { health, position: { x, y } };
+  }
+};
+
+encodeProjectiles = () => {
+  var payload = {};
+  for (let id in projectiles) {
+    payload[id] = encode(projectiles[id], "bullet");
+  }
+  return payload;
+};
+
+var projectiles;
+
+var encode = (object, type) => {
+  if (type == "bullet") {
+    let { position } = object;
+    return [position.x, position.y];
+  } else if (type == "player") {
+    let { health, position } = object;
+    return [health, position.x, position.y];
+  }
+};
+
+var generateID = () => {
+  //create 8 digit random id
+  let str = "";
+  for (let i = 0; i < 5; i++) {
+    if (i % 2 == 0) str += Math.floor(Math.random() * 9);
+    else str += String.fromCharCode(Math.floor(Math.random() * (89 - 65) + 65));
+  }
+  if (projectiles[str]) generateID();
+  else return str;
+};
